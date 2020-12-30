08f7c4ecc2af371e15c58a39cf45157e5a22f9ec
Peter <petrk@wix.com>
2020-12-30 21:13:02 +0200

    feat: add copy to santa

--- src/bin.ts
+++ src/bin.ts
@@ -3,9 +3,12 @@
 import createDebug from 'debug';
 import { writeFileSync } from 'fs';
 import { sync } from 'glob';
+import mkdirp from 'mkdirp';
+import { join } from 'path';
 import * as yargs from 'yargs';
 import { Arguments, Argv } from 'yargs';
 import { certificateFor } from './index';
+
 
 const debug = createDebug('devcert');
 
@@ -16,16 +19,20 @@ yargs
     (yargs: Argv) =>
       yargs
         .positional('domain', {
-          describe: 'domain certificate'
+          describe: 'domain certificate',
         })
         .option('skip-hosts-file', {
           describe:
-            "If present will skip adding domain to hosts file if it doesn't exists",
-          type: 'boolean'
+            'If present will skip adding domain to hosts file if it doesn\'t exists',
+          type: 'boolean',
         })
         .option('copy-to-yoshi', {
           describe: 'output keys to yoshi project',
-          type: 'boolean'
+          type: 'boolean',
+        })
+        .option('copy-to-santa', {
+          describe: 'output keys to santa config',
+          type: 'boolean',
         }),
     async (argv: Arguments) => {
       const { key, cert } = await certificateFor(argv.domain, {
@@ -43,6 +50,14 @@ yargs
             debug(`Copied key to ${filePath}`);
             writeFileSync(filePath, key, { encoding: 'utf8' });
           });
+      }
+      if (argv.copyToSanta) {
+        const dirPath = join(process.env.HOME, '.config/configstore', 'santa', 'sslcert');
+        mkdirp.sync(dirPath);
+        writeFileSync(join(dirPath, 'cert.pem'), cert, { encoding: 'utf8' });
+        writeFileSync(join(dirPath, 'key.pem'), key, { encoding: 'utf8' });
+
+        debug('copied to santa');
       }
     }
   )
